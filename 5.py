aa594e2748bbc1326ed16d48c7103855bf5868e2
PatelDharini <102034538+PatelDharini@users.noreply.github.com>
2022-09-21 02:58:54 +0530

    5
    here is same task to find positive elements from list but with the list provided from programmer

---
+++ 5.py
@@ -0,0 +1,12 @@
+#to print all the positive numbers from the provided list
+list1=[12,-7,5,64,-14]
+print("output:",end="")
+for i in list1:
+    if i>=0:
+        print(i, end="  ")
+print("\n")
+list2=[12,14,-95,3]
+print("output:",end="")
+for i in list2:
+    if i>=0:
+        print(i, end="  ")
