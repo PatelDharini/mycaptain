527f83764e45084d9f41e2c9c75587c6ae83495c
PatelDharini <102034538+PatelDharini@users.noreply.github.com>
2022-09-21 01:08:25 +0530

    1
    this will print the area of circle through the radius provided by user

---
+++ 1.py
@@ -0,0 +1,6 @@
+#program to find area of circle
+import math
+print("calculating for circle")
+r=float(input("Enter the radius of circle:"))
+a=math.pi*r
+print("The area of circle whith radius",r,"is:",a)
